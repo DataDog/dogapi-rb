1c3a9167fb7ebe7776bd1337a8c62149918d807f
Matt Perpick <matthewperpick@gmail.com>
2012-08-31 14:13:10 -0400

    Share test setup between modules.

--- tests/test_client.rb
+++ tests/test_client.rb
@@ -1,21 +1,8 @@
-require 'test/unit'
 require 'dogapi'
 require 'time'
+require_relative 'test_base.rb'
 
-class TestClient < Test::Unit::TestCase
-
-  def config_client_test_env
-    @api_key = ENV['DATADOG_API_KEY']
-    @app_key = ENV['DATADOG_APP_KEY']
-    if not @api_key or not @app_key
-      puts "\n"
-      abort "To run tests in your environment, set 'DATADOG_API_KEY' and 'DATADOG_APP_KEY' to appropriate values for your account. Be aware that the tests will submit data, some of which won't be removed at the end.\n\n"
-    end
-  end
-
-  def setup
-    config_client_test_env()
-  end
+class TestClient < TestBase
 
   def test_tags
     hostname = 'test.tag.host'
--- tests/test_comments.rb
+++ tests/test_comments.rb
@@ -1,21 +1,8 @@
-require 'test/unit'
 require 'dogapi'
 require 'time'
+require_relative 'test_base.rb'
 
-class TestComments < Test::Unit::TestCase
-
-  def config_client_test_env
-    @api_key = ENV['DATADOG_API_KEY']
-    @app_key = ENV['DATADOG_APP_KEY']
-    if not @api_key or not @app_key
-      puts "\n"
-      abort "To run tests in your environment, set 'DATADOG_API_KEY' and 'DATADOG_APP_KEY' to appropriate values for your account. Be aware that the tests will submit data, some of which won't be removed at the end.\n\n"
-    end
-  end
-
-  def setup
-    config_client_test_env()
-  end
+class TestComments < TestBase
 
   def test_comments
     dog = Dogapi::Client.new(@api_key, @app_key)

c76264cf73f887d4fdcf903d4912bc694407f7b5
Seth Rosenblum <seth@datadoghq.com>
2013-09-04 20:19:41 +0000

    Move mocks to before(:each) just for the tests they affect.

--- spec/facade_spec.rb
+++ spec/facade_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "Facade", :vcr => true do
 
-  before(:each) do
+  before(:all) do
     @api_key = ENV["DATADOG_API_KEY"]
     @app_key = ENV["DATADOG_APP_KEY"]
     @job_number = ENV['TRAVIS_JOB_NUMBER'] || '1'
@@ -11,33 +11,33 @@ describe "Facade", :vcr => true do
 
   context "Client" do
 
+    before(:each) do
+      @dogmock = Dogapi::Client.new(@api_key, @app_key)
+      @metric_svc = double
+      @dogmock.instance_variable_set("@metric_svc", @metric_svc)
+    end
+
     it "emit_point passes data" do
-      metric_svc = double
-      @dog.instance_variable_set("@metric_svc", metric_svc)
-      metric_svc.should_receive(:submit) do |metric, points, scope, options|
+      @metric_svc.should_receive(:submit) do |metric, points, scope, options|
         expect(metric).to eq "metric.name"
         expect(points[0][1]).to eq 0
         expect(scope.host).to eq "myhost"
       end
-      @dog.emit_point("metric.name", 0, :host => "myhost")
+      @dogmock.emit_point("metric.name", 0, :host => "myhost")
     end
 
     it "emit_point uses localhost default" do
-      metric_svc = double
-      @dog.instance_variable_set("@metric_svc", metric_svc)
-      metric_svc.should_receive(:submit) do |metric, points, scope, options|
+      @metric_svc.should_receive(:submit) do |metric, points, scope, options|
         expect(scope.host).to eq Dogapi.find_localhost
       end
-      @dog.emit_point("metric.name", 0)
+      @dogmock.emit_point("metric.name", 0)
     end
 
     it "emit_point can pass nil host" do
-      metric_svc = double
-      @dog.instance_variable_set("@metric_svc", metric_svc)
-      metric_svc.should_receive(:submit) do |metric, points, scope, options|
+      @metric_svc.should_receive(:submit) do |metric, points, scope, options|
         expect(scope.host).to be_nil
       end
-      @dog.emit_point("metric.name", 0, :host => nil)
+      @dogmock.emit_point("metric.name", 0, :host => nil)
     end
 
   end

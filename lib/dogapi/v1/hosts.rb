c994d1e9960c63557a846f2d581ab1cb551359de
samanthadrago <samantha@datadoghq.com>
2018-04-13 13:12:10 -0400

    Add hosts API

---
+++ lib/dogapi/v1/hosts.rb
@@ -0,0 +1,20 @@
+require 'dogapi'
+
+module Dogapi
+  class V1 # for namespacing
+
+    class HostsService < Dogapi::APIService
+
+      API_VERSION = 'v1'
+
+      def get_all(options = {})
+        request(Net::HTTP::Get, "/api/#{API_VERSION}/hosts", nil, options, true)
+      end
+
+      def totals()
+        request(Net::HTTP::Get, "/api/#{API_VERSION}/hosts/totals", nil, nil, true)
+      end
+    end
+
+  end
+end

20721eaa986af42700142a4d6a23eb045acedda3
samanthadrago <samantha@datadoghq.com>
2018-04-16 15:04:34 -0400

    Fix rubocop issues

--- lib/dogapi/v1/hosts.rb
+++ lib/dogapi/v1/hosts.rb
@@ -3,6 +3,7 @@ require 'dogapi'
 module Dogapi
   class V1 # for namespacing
 
+    # Hosts API
     class HostsService < Dogapi::APIService
 
       API_VERSION = 'v1'
@@ -11,7 +12,7 @@ module Dogapi
         request(Net::HTTP::Get, "/api/#{API_VERSION}/hosts", nil, options, true)
       end
 
-      def totals()
+      def totals
         request(Net::HTTP::Get, "/api/#{API_VERSION}/hosts/totals", nil, nil, true)
       end
     end

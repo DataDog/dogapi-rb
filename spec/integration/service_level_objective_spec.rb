5ef5cdf3d4ec71398e1a03473b8d314b2d388c87
Cody Lee <cody.lee@datadoghq.com>
2019-10-01 10:23:21 -0500

    small tweak

--- spec/integration/service_level_objective_spec.rb
+++ spec/integration/service_level_objective_spec.rb
@@ -11,25 +11,25 @@ describe Dogapi::Client do
   SLO_THRESHOLDS = [{:timeframe => "7d", :target => 90}, {:timeframe => "30d", :target=> 95}].freeze
 
   describe '#create_service_level_objective' do
-    it_behaves_like 'an api method with options',
+    it_behaves_like 'an api method',
                     :create_service_level_objective, [SLO_TYPE, SLO_NAME, SLO_DESCRIPTION, SLO_TAGS, SLO_THRESHOLDS, SLO_QUERY_NUMERATOR, SLO_QUERY_DENOMINATOR],
                     :post, '/slo', 'type' => SLO_TYPE, 'name' => SLO_NAME
   end
 
   describe '#update_service_level_objective' do
-    it_behaves_like 'an api method with options',
+    it_behaves_like 'an api method',
                     :update_service_level_objective, [SLO_ID, SLO_TYPE, SLO_NAME],
                     :put, "/slo/#{SLO_ID}", 'type' => SLO_TYPE, 'name' => SLO_NAME
   end
 
   describe '#get_service_level_objective' do
-    it_behaves_like 'an api method with optional params',
+    it_behaves_like 'an api method',
                     :get_service_level_objective, [SLO_ID],
                     :get, "/slo/#{SLO_ID}"
   end
 
   describe '#search_service_level_objective' do
-    it_behaves_like 'an api method with optional params',
+    it_behaves_like 'an api method',
                     :search_service_level_objective, [[SLO_ID]],
                     :get, "/slo", 'ids' => [SLO_ID], 'offset' => 0, 'limit' => 100
   end

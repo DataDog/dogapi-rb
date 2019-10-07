377c1721717457a8df3a4b2d77780c84bddffa12
Ricky Thomas <ricky@datadoghq.com>
2019-10-07 16:39:00 -0400

    Update GCP integration with docs and refactor

--- lib/dogapi/v1/azure_integration.rb
+++ lib/dogapi/v1/azure_integration.rb
@@ -3,7 +3,7 @@ require 'dogapi'
 module Dogapi
   class V1 # for namespacing
 
-    # AwsIntegrationService for user interaction with AWS configs.
+    # AzureIntegrationService for user interaction with Azure configs.
     class AzureIntegrationService < Dogapi::APIService
 
       API_VERSION = 'v1'
@@ -13,34 +13,67 @@ module Dogapi
         request(Net::HTTP::Get, "/api/#{API_VERSION}/integration/azure", nil, nil, false)
       end
 
-      # Create an AWS integration
+      # Delete an Azure integration
       # :config => Hash: integration config.
-    #   def create_aws_integration(config)
-    #     request(Net::HTTP::Post, "/api/#{API_VERSION}/integration/aws", nil, config, true)
-    #   end
+      # config = {
+      #   :tenant_name => '<TENANT_NAME>',
+      #   :client_id => '<CLIENT_ID>'
+      # }
+      #
+      # dog = Dogapi::Client.new(api_key, app_key)
+      #
+      # puts dog.delete_azure_integration(config)
+      def delete_azure_integration(config)
+        request(Net::HTTP::Delete, "/api/#{API_VERSION}/integration/azure", nil, config, true)
+      end
 
-      # Delete an integration
+      # Create an Azure integration
       # :config => Hash: integration config.
-    #   def delete_aws_integration(config)
-    #     request(Net::HTTP::Delete, "/api/#{API_VERSION}/integration/aws", nil, config, true)
-    #   end
+      # config = {
+      #   :tenant_name => '<TENANT_NAME>',
+      #   :host_filters => 'new:filter',
+      #   :client_id => '<CLIENT_ID>',
+      #   :client_secret => '<CLIENT_SECRET>'
+      # }
+      #
+      # dog = Dogapi::Client.new(api_key, app_key)
+      #
+      # puts dog.create_azure_integration(config)
+      def create_azure_integration(config)
+        request(Net::HTTP::Post, "/api/#{API_VERSION}/integration/azure", nil, config, true)
+      end
 
-      # List available AWS namespaces
-    #   def list_aws_namespaces
-    #     request(Net::HTTP::Get, "/api/#{API_VERSION}/integration/aws/available_namespace_rules", nil, nil, false)
-    #   end
-
-      # Generate new AWS external ID for a specific integrated account
+      # Update an Azure integrations host filters
       # :config => Hash: integration config.
-    #   def generate_external_id(config)
-    #     request(Net::HTTP::Put, "/api/#{API_VERSION}/integration/aws/generate_new_external_id", nil, config, true)
-    #   end
+      # config = {
+      #   :tenant_name => '<TENANT_NAME>',
+      #   :host_filters => 'new:filter',
+      #   :client_id => '<CLIENT_ID>'
+      # }
+      #
+      # dog = Dogapi::Client.new(api_key, app_key)
+      #
+      # puts dog.update_azure_host_filters(config)
+      def update_azure_host_filters(config)
+        request(Net::HTTP::Post, "/api/#{API_VERSION}/integration/azure/host_filters", nil, config, true)
+      end
 
-      # Update integrated AWS account
-      # :config => Hash: integration config. Not working atm
-    #   def update_aws_account(config)
-    #     request(Net::HTTP::Put, "/api/#{API_VERSION}/integration/aws", nil, config, true)
-    #   end
+      # Update a configured Azure account.
+      # :config => Hash: integration config.
+      # config = {
+      #   :tenant_name => '<TENANT_NAME>',
+      #   :new_tenant_name => '<NEW_TENANT_NAME>',
+      #   :host_filters => '<KEY>:<VALUE>,<KEY>:<VALUE>',
+      #   :client_id => '<CLIENT_ID>',
+      #   :new_client_id => '<NEW_CLIENT_ID>'
+      # }
+      #
+      # dog = Dogapi::Client.new(api_key, app_key)
+      #
+      # puts dog.update_azure_integration(config)
+      def update_azure_integration(config)
+        request(Net::HTTP::Put, "/api/#{API_VERSION}/integration/azure", nil, config, true)
+      end
 
     end
 

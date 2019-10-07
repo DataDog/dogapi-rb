e87b35eaf62790655869f20c91636c93b6940ad3
Ricky Thomas <ricky@datadoghq.com>
2019-10-07 16:38:30 -0400

    Update GCP integration with docs and refactor

--- lib/dogapi/v1/gcp_integration.rb
+++ lib/dogapi/v1/gcp_integration.rb
@@ -15,27 +15,56 @@ module Dogapi
 
       # Delete an gcp integration
       # :config => Hash: integration config.
-    #   def delete_gcp_integration(config)
-    #     request(Net::HTTP::Delete, "/api/#{API_VERSION}/integration/gcp", nil, config, true)
-    #   end
+      # config = {
+      #   :project_id => 'datadog-sandbox',
+      #   :client_email => 'ricky-api-dev@datadog-sandbox.iam.gserviceaccount.com'
+      # }
+      #
+      # dog = Dogapi::Client.new(api_key, app_key)
+      #
+      # puts dog.delete_gcp_integration(config)
+      def delete_gcp_integration(config)
+        request(Net::HTTP::Delete, "/api/#{API_VERSION}/integration/gcp", nil, config, true)
+      end
 
       # Create an gcp integration
       # :config => Hash: integration config.
-    #   def create_gcp_integration(config)
-    #     request(Net::HTTP::Post, "/api/#{API_VERSION}/integration/gcp", nil, config, true)
-    #   end
-
-      # Update an gcp integrations host filters
-      # :config => Hash: integration config.
-    #   def update_gcp_host_filters(config)
-    #     request(Net::HTTP::Post, "/api/#{API_VERSION}/integration/gcp/host_filters", nil, config, true)
-    #   end
+      # config = {
+      #   :type => 'service_account',
+      #   :project_id => '<PROJECT_ID>',
+      #   :private_key_id => '<PRIVATE_KEY_ID>',
+      #   :private_key => '<PRIVATE_KEY>',
+      #   :client_email => '<CLIENT_EMAIL>',
+      #   :client_id => '<CLIENT_ID>',
+      #   :auth_uri => '<AUTH_URI>',
+      #   :token_uri => '<TOKEN_URI>',
+      #   :auth_provider_x509_cert_url => '<AUTH_PROVIDER_X509_CERT_URL>',
+      #   :client_x509_cert_url => '<CLIENT_X509_CERT_URL>',
+      #   :host_filters => '<KEY>:<VALUE>,<KEY>:<VALUE>,'
+      # }
+      #
+      # dog = Dogapi::Client.new(api_key, app_key)
+      #
+      # puts dog.create_gcp_integration(config)
+      def create_gcp_integration(config)
+        request(Net::HTTP::Post, "/api/#{API_VERSION}/integration/gcp", nil, config, true)
+      end
 
       # Update a configured gcp account.
       # :config => Hash: integration config.
-    #   def update_gcp_integration(config)
-    #     request(Net::HTTP::Put, "/api/#{API_VERSION}/integration/gcp", nil, config, true)
-    #   end
+      # config = {
+      #   :project_id => '<PROJECT_ID>',
+      #   :client_email => '<CLIENT_EMAIL>',
+      #   :host_filters => '<KEY>:<VALUE>,<KEY>:<VALUE>,'
+      #   :automute => true # takes a boolean and toggles GCE automuting
+      # }
+      #
+      # dog = Dogapi::Client.new(api_key, app_key)
+      #
+      # puts dog.update_gcp_integration(config)
+      def update_gcp_integration(config)
+        request(Net::HTTP::Put, "/api/#{API_VERSION}/integration/gcp", nil, config, true)
+      end
 
     end
 

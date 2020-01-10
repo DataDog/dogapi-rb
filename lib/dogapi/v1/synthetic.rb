684f5d0183377177471b6af686dd141ea11f90f6
David Bouchare <david.bouchare@datadoghq.com>
2020-01-10 12:03:50 +0100

    Add support for a few synthetics endpoint

--- lib/dogapi/v1/synthetic.rb
+++ lib/dogapi/v1/synthetic.rb
@@ -7,7 +7,7 @@ module Dogapi
 
       API_VERSION = 'v1'
 
-      def synthetic(type, config, options = {})
+      def create_test(type, config, options = {})
         body = {
           'type' => type,
           'config' => config
@@ -16,19 +16,39 @@ module Dogapi
         request(Net::HTTP::Post, "/api/#{API_VERSION}/synthetics/tests", nil, body, true)
       end
 
-      def update_synthetic(synthetic_id, config, options = {})
+      def update_test(test_id, config, options = {})
         body = {
           'config' => config
         }.merge(options)
 
-        request(Net::HTTP::Put, "/api/#{API_VERSION}/synthetics/tests/#{synthetic_id}", nil, body, true)
+        request(Net::HTTP::Put, "/api/#{API_VERSION}/synthetics/tests/#{test_id}", nil, body, true)
       end
 
-      def get_all_synthetics
+      def get_all_tests
         request(Net::HTTP::Get, "/api/#{API_VERSION}/synthetics/tests", nil, nil, false)
       end
 
-    end
+      def get_test(test_id)
+        request(Net::HTTP::Get, "/api/#{API_VERSION}/synthetics/tests/#{test_id}", nil, nil, false)
+      end
 
+      def get_results(test_id)
+        request(Net::HTTP::Get, "/api/#{API_VERSION}/synthetics/tests/#{test_id}/results", nil, nil, false)
+      end
+
+      def get_result(test_id, result_id)
+        request(Net::HTTP::Get, "/api/#{API_VERSION}/synthetics/tests/#{test_id}/results/#{result_id}", nil, nil, false)
+      end
+
+      def get_devices
+        request(Net::HTTP::Get, "/api/#{API_VERSION}/synthetics/browser/devices", nil, nil, false)
+      end
+
+      def get_locations
+        request(Net::HTTP::Get, "/api/#{API_VERSION}/synthetics/locations", nil, nil, false)
+      end
+
+    end
+    
   end
 end

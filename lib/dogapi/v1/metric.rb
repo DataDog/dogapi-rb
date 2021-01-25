e2ef28508d7b3de1750f4f08238e63bed80451e9
Hippolyte HENRY <zippolyte@users.noreply.github.com>
2021-01-25 15:38:58 +0100

    Remove circular dependency (#253)
    * Remove circular dependency
    
    * pin crack

--- lib/dogapi/v1/metric.rb
+++ lib/dogapi/v1/metric.rb
@@ -2,8 +2,6 @@
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2011-Present Datadog, Inc.
 
-require 'dogapi'
-
 module Dogapi
   class V1 # for namespacing
 

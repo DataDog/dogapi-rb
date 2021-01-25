82228be13db0f9aeaca31abee793f31b63b56de2
Hippolyte HENRY <hippolyte.henry@datadoghq.com>
2021-01-25 13:38:05 +0100

    Remove circular dependency

--- lib/dogapi/v1/service_level_objective.rb
+++ lib/dogapi/v1/service_level_objective.rb
@@ -1,4 +1,6 @@
-require 'dogapi'
+# Unless explicitly stated otherwise all files in this repository are licensed under the BSD-3-Clause License.
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2011-Present Datadog, Inc.
 
 module Dogapi
   class V1 # for namespacing
